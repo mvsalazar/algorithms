dca696574c557e0b27a63bb2ef72fba2436b28a9
Mel Salazar <melvin.v.salazar@gmail.com>
2025-09-26 16:26:32 -0700

    Added more efficient solution to SquaringSortedArray

--- src/main/java/com/interview/prep/algorithms/SquaringSortedArray.java
+++ src/main/java/com/interview/prep/algorithms/SquaringSortedArray.java
@@ -11,6 +11,9 @@ import java.util.*;;
  * Output: [0, 1, 4, 4, 9]
  */
 public class SquaringSortedArray {
+    /**
+     * Brute force approach with time complexity O(n log n)
+     */
     public static int[] solution(int[] arr) {
         int[] squaredArray = new int[arr.length]; 
 
@@ -24,10 +27,44 @@ public class SquaringSortedArray {
         return squaredArray;
     }
 
+    /**
+     * More efficient with single pass an time complexity of O(n)
+     */
+    public static int[] solution2(int[] arr) {
+        // We can use two pointers
+        int n = arr.length;
+        int [] squares = new int[n];
+        int highestSquareIndex = n - 1;
+        int left = 0;
+        int right = n - 1;
+
+        while(left <= right) {
+            int leftSquare = arr[left] * arr[left];
+            int rightSquare = arr[right] * arr[right];
+
+            if(leftSquare > rightSquare) {
+                squares[highestSquareIndex--] = leftSquare;
+                left++;
+            } else {
+                squares[highestSquareIndex--] = rightSquare;
+                right--;
+            }           
+        }
+
+        return squares;
+    }
+
     public static void main(String[] args) {
         int[] arr = new int[]{-2, -1, 0, 2, 3};
         int[] result = solution(arr);
+        int[] result2 = solution2(arr);
 
         System.out.println(Arrays.toString(result));
+        System.out.println();
+        System.out.println(Arrays.toString(result2));
+
+        // [0, 1, 4, 4, 9]
+        //
+        // [0, 1, 4, 4, 9]
     }
 }
